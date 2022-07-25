5c5554480251ace13f6f85e9dd3830a8a5425e99
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-07-25 12:21:44 +0900

    20220725

---
+++ react00/src/App.js
@@ -0,0 +1,17 @@
+import Test from "./test";
+import Nav from "./Nav";
+const App = () => {
+  //console.log('하하하')
+  return (
+    <>
+      <Test />
+      <h1>호호호</h1>
+      <Nav />
+      <Test />
+      <Test />
+      {/* <h2>hahahaha</h2> */}
+    </>
+  )
+}
+
+export default App;
---
+++ react00/src/test.js
@@ -0,0 +1,12 @@
+import App from "./App"
+
+function Test() {
+    return (
+        <>
+            <i className="xi-bars"></i>
+            <p>Lorem ipsum dolor sit.</p>
+        </>
+    )
+}
+
+export default Test;
